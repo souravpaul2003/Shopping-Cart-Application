261c291ffe4c9d2e28d75ba0b7f0b5f1805a63b9
Sourav Paul <140735141+souravpaul2003@users.noreply.github.com>
2024-10-07 17:54:01 +0530

    Add files via upload

---
+++ shoppingkartproject/src/shoppingkartproject/ShoppingKart.java
@@ -0,0 +1,15 @@
+package shoppingkartproject;
+
+//This class is the main entry point 
+
+public class ShoppingKart {
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t~~ Welcome To Shopping Kart ~~");
+		ShoppingKartOperations.AllMenus();
+
+	}
+	
+}
